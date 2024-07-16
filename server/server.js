c7f27020971d56e892da130e10f689491e49cb93
sagar-blok <sagar@blokument.com>
2024-07-17 03:12:34 +0530

    fix: installed cors for cross origin errors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
-app.get("api/home", (req, res) =>{
+app.use(cors());
+
+app.get("/api/home", (req, res) =>{
     res.json({ message: "API is working properly" });
 })
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
