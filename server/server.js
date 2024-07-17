995689593bb7ae25de7d266f233bb5884067ef29
sagar-blok <sagar@blokument.com>
2024-07-17 11:17:07 +0530

    chore: add header security libraries

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => {
-    res.json({ message: "API is working properly" });
+app.use(helmet());
+app.use(xss());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.json({ message: 'API is working properly' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
